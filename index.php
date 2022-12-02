a626f89f789ec5fda40be17eb49f61fdc3cdcb12
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-12-02 16:06:26 +0100

    first

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Generatore Password</title>
+
+    <style>
+        body{
+            background-color:#e5e5e5;
+            color: #fca311;
+        }
+        h2{
+            font-size:3rem;
+        }
+        main{
+            height: 100vh;
+        }
+        .container-sm{
+            height: 50%;
+            position: absolute;
+            top:0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            margin: auto;
+            width: 65vw;
+            border-radius: 30px;
+            background-color: #14213d;
+            padding: 20px;
+        }
+    </style>
+
+</head>
+<body>
+   <main class="">
+        <div class="container-sm ">
+            <h2 class="">ciao</h2>
+        </div>
+   </main>
+    
+</body>
+</html>
